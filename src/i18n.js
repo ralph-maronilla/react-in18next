6efc7e06fb401dcff2241837de9178031926218d
ralph-maronilla <ralph.maronilla@davidandgolyat.com>
2023-08-11 11:03:11 +0800

    langauge default back to english

--- src/i18n.js
+++ src/i18n.js
@@ -20,7 +20,7 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    fallbackLng: "kr",
+    fallbackLng: "en",
     debug: true,
     detection: {
       order: ["queryString", "cookie"],
